669c01543a071778d2d1debec2a32a0f9200dc55
HelgaTe <telizhuk@gmail.com>
2022-05-31 23:04:29 +0300

    executed tasks

--- exercises/06_control_structures/task_6_2a.py
+++ exercises/06_control_structures/task_6_2a.py
@@ -17,3 +17,34 @@
 
 Ограничение: Все задания надо выполнять используя только пройденные темы.
 """
+
+ip=input('Enter IP-address : ')
+octets = ip.split('.')
+
+# если разделителем являеться точка, то метод выдаст элементы (ровно 4) для
+# дальнейшей проверки; если разделитель не точка, метод split() отработает
+# некорректно и будем иметь дело с одним элементом
+
+correct_ip=True
+
+if len(octets)!=4: # проверить что адрес состоит из 4-х элементов
+    correct_ip=False # если не правда -> состоит не из 4-х элементов -> продолжаем проверку дальше
+else:
+    for item in octets:
+        if not(item.isdigit() and int(item) in range(256)): #проверка, что состоит из 4 чисел и каждое число в диапазоне от 0 до 255
+            correct_ip=False # если не правда -> условия не выполнены, просим повторно внести ip-address (блок ниже)
+            break
+if not correct_ip:
+    print('Неправильный IP-адрес')
+    ip=input('Enter IP-address : ')
+else:
+    if int(octets[0]) in range(1,224):
+        print('unicast')
+    elif int(octets[0]) in range(224,240):
+        print('multicast')
+    elif ip=='255.255.255.255':
+        print('local broadcast')
+    elif ip=='0.0.0.0':
+        print('unassigned')
+    else:
+        print('unused')
--- exercises/06_control_structures/task_6_2b.py
+++ exercises/06_control_structures/task_6_2b.py
@@ -12,3 +12,30 @@
 
 Ограничение: Все задания надо выполнять используя только пройденные темы.
 """
+
+while True:
+        ip=input('Enter IP-address: ')
+        octets=ip.split('.')
+        correct_ip=True
+
+        if len(octets) ==4:
+                for octet in octets:
+                        if not (octet.isdigit() and int(octet) in range(256)):
+                                correct_ip=False
+                                break
+        else:
+                correct_ip=False
+        if correct_ip:
+                break
+        print("Неправильный IP-адрес")
+        
+if int(octets[0]) in range(1,224):
+        print('unicast')
+elif int(octets[0]) in range(224, 240):
+        print('multicast')
+elif ip == '255.255.255.255':
+        print('local broadcast')
+elif ip == '0.0.0.0':
+        print('unassigned')
+else:
+        print('unused')
